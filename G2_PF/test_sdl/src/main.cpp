06a47ef691f5ec57533c00bbeefc42b5ffd3aa04
Samuel <samuebla@ucm.es>
2023-05-14 18:31:34 +0200

    Primera interfaz visualizada

--- G2_PF/test_sdl/src/main.cpp
+++ G2_PF/test_sdl/src/main.cpp
@@ -1,3 +1,6 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 #include <ianium/testable_ui/button.h>
 #include <ianium/testable_ui/slider.h>
 #include <ianium/testable_ui/toggle.h>
@@ -10,8 +13,24 @@
 
 class Button : public IAButton {
 public:
-	Button(int id, int posX, int posY, int w, int h, bool active, const char* menu) : IAButton(id, posX, posY, w, h, active, menu) {};
+	Button(int id, int posXAux, int posYAux, int wAux, int hAux, bool active, const char* menu) : IAButton(id, posX, posY, w, h, active, menu) {
+		posX = posXAux;
+		posY = posYAux;
+		w = wAux;
+		h = hAux;
+	};
 	~Button() = default;
+
+	int posX, posY, w, h;
+
+	SDL_Rect rect;
+	void render(SDL_Renderer* renderer) {
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+
+		rect = { posX,posY,w,h };
+		SDL_RenderFillRect(renderer, &rect);
+	}
 };
 
 class Slider : public IASlider {
@@ -20,33 +39,50 @@ public:
 		const float value, const float minValue, const float maxValue, const int rangeSelection, const Orientation orientation)
 		:IASlider(id, posX, posY, w, h, active, menu, value, minValue, maxValue, rangeSelection, orientation) {};
 	~Slider() = default;
+
+	void render(SDL_Renderer* renderer) {
+		// Lógica de renderizado del elemento UI
+	}
 };
 
 class Toogle : public IAToggle {
 public:
 	Toogle(const int id, const int posX, const int posY, const int w, const int h, const bool active, const char* menu) : IAToggle(id, posX, posY, w, h, active, menu) {};
 	~Toogle() = default;
+
+	void render(SDL_Renderer* renderer) {
+		// Lógica de renderizado del elemento UI
+	}
 };
 
 
 int main() {
+
+	// Inicializar SDL
+	SDL_Init(SDL_INIT_VIDEO);
+
+	// Creamos la ventana
+	SDL_Window* window = SDL_CreateWindow("Juego", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+
+	// Creamos un renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
 	Ianium::Init("NOT NEEDED RIGHT NOW CHANGE THIS");
 
-	std::list<UIElement> hud;
-	
-	//Interfaz
-	Button a = Button(0, 0, 0, 0, 0, 0, "u");
-	hud.push_back(a);
-	Button b = Button(1, 0, 0, 0, 0, 0, "e");
-	hud.push_back(b);
-	Button c = Button(2, 0, 0, 0, 0, 0, "4");
-	hud.push_back(c);
-	
-	Toogle t = Toogle(3, 10, 10, 10, 10, true, "4");
-	hud.push_back(t);
-	Slider s = Slider(4, 40, 40, 20, 20, true, "4", 20.0, 0.0, 50.0, 50, IASlider::Orientation::HORIZONTAL);
-	hud.push_back(s);
+	std::list<Button> hud;
 
+	//Interfaz
+	Button a = Button(0, 10,10, 30, 30, true, "u");
+	hud.push_back(a);
+	Button b = Button(1, 60, 0, 60, 60, true, "e");
+	hud.push_back(b);
+	Button c = Button(2, 0, 70, 20, 20, true, "4");
+	hud.push_back(c);
+
+	//Toogle t = Toogle(3, 10, 10, 10, 10, true, "4");
+	//hud.push_back(t);
+	//Slider s = Slider(4, 40, 40, 20, 20, true, "4", 20.0, 0.0, 50.0, 50, IASlider::Orientation::HORIZONTAL);
+	//hud.push_back(s);
 
 	Ianium::Instance()->visualTesting->testOPENCV("./tempAssets/testImage.jpg");
 
@@ -64,6 +100,9 @@ int main() {
 				if (event.type == SDL_QUIT) {
 					gameRunning = false;
 				}
+				else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+					gameRunning = false;
+				}
 			}
 
 			// Renderizar elementos
@@ -71,9 +110,9 @@ int main() {
 			SDL_RenderClear(renderer);
 
 			//Renderitamos los elementos de la interfaz
-			for(UIElement elem: hud)
+			for (Button elem : hud)
 			{
-
+				elem.render(renderer);
 			}
 
 			//Y lo muestra en pantalla
