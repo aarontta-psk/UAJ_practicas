b283b1cd7421155b379f674733fb58b81339c84d
Samuel <samuebla@ucm.es>
2023-05-14 18:43:54 +0200

    Boton y toggle implementados in-game

--- G2_PF/test_sdl/src/main.cpp
+++ G2_PF/test_sdl/src/main.cpp
@@ -24,8 +24,10 @@ public:
 	int posX, posY, w, h;
 
 	SDL_Rect rect;
+
 	void render(SDL_Renderer* renderer) {
 
+		//Cuadrado rosita
 		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 
 		rect = { posX,posY,w,h };
@@ -35,23 +37,66 @@ public:
 
 class Slider : public IASlider {
 public:
-	Slider(const int id, const int posX, const int posY, const int w, const int h, const bool active, const char* menu,
+	Slider(const int id, const int posXAux, const int posYAux, const int wAux, const int hAux, const bool active, const char* menu,
 		const float value, const float minValue, const float maxValue, const int rangeSelection, const Orientation orientation)
-		:IASlider(id, posX, posY, w, h, active, menu, value, minValue, maxValue, rangeSelection, orientation) {};
+		:IASlider(id, posX, posY, w, h, active, menu, value, minValue, maxValue, rangeSelection, orientation) {
+		posX = posXAux;
+		posY = posYAux;
+		w = wAux;
+		h = hAux;
+	};
 	~Slider() = default;
 
+	int posX, posY, w, h;
+	Orientation orientation;
+	SDL_Rect rect;
+
 	void render(SDL_Renderer* renderer) {
-		// Lógica de renderizado del elemento UI
+
+		SDL_SetRenderDrawColor(renderer, 100, 200, 255, 255);
+
+		if (orientation == Orientation::VERTICAL) {
+
+		}
+		else {
+
+
+		}
+
+
+		rect = { posX,posY,w,h };
+		SDL_RenderFillRect(renderer, &rect);
 	}
 };
 
-class Toogle : public IAToggle {
+class Toggle : public IAToggle {
 public:
-	Toogle(const int id, const int posX, const int posY, const int w, const int h, const bool active, const char* menu) : IAToggle(id, posX, posY, w, h, active, menu) {};
-	~Toogle() = default;
+	Toggle(const int id, const int posXAux, const int posYAux, const int wAux, const int hAux, const bool active, const char* menu) : IAToggle(id, posX, posY, w, h, active, menu) {
+		posX = posXAux;
+		posY = posYAux;
+		w = wAux;
+		h = hAux;
+		toogleOn = true;
+	};
+	~Toggle() = default;
+
+	int posX, posY, w, h;
+	SDL_Rect rect;
+	bool toogleOn;
 
 	void render(SDL_Renderer* renderer) {
-		// Lógica de renderizado del elemento UI
+
+		//Activao
+		if (toogleOn)
+			//Verde
+			SDL_SetRenderDrawColor(renderer, 0, 220, 10, 255);
+		//Desactivado
+		else
+			//Rojo
+			SDL_SetRenderDrawColor(renderer, 220, 0, 10, 255);
+
+		rect = { posX,posY,w,h };
+		SDL_RenderFillRect(renderer, &rect);
 	}
 };
 
@@ -69,18 +114,18 @@ int main() {
 
 	Ianium::Init("NOT NEEDED RIGHT NOW CHANGE THIS");
 
-	std::list<Button> hud;
+	std::list<Toggle> hud;
 
 	//Interfaz
-	Button a = Button(0, 10,10, 30, 30, true, "u");
-	hud.push_back(a);
-	Button b = Button(1, 60, 0, 60, 60, true, "e");
-	hud.push_back(b);
-	Button c = Button(2, 0, 70, 20, 20, true, "4");
-	hud.push_back(c);
+	//Button a = Button(0, 10, 10, 30, 30, true, "u");
+	//hud.push_back(a);
+	//Button b = Button(1, 60, 0, 60, 60, true, "e");
+	//hud.push_back(b);
+	//Button c = Button(2, 0, 70, 20, 20, true, "4");
+	//hud.push_back(c);
 
-	//Toogle t = Toogle(3, 10, 10, 10, 10, true, "4");
-	//hud.push_back(t);
+	Toggle t = Toggle(3, 500,300,100,100, true, "4");
+	hud.push_back(t);
 	//Slider s = Slider(4, 40, 40, 20, 20, true, "4", 20.0, 0.0, 50.0, 50, IASlider::Orientation::HORIZONTAL);
 	//hud.push_back(s);
 
@@ -110,7 +155,7 @@ int main() {
 			SDL_RenderClear(renderer);
 
 			//Renderitamos los elementos de la interfaz
-			for (Button elem : hud)
+			for (Toggle elem : hud)
 			{
 				elem.render(renderer);
 			}
