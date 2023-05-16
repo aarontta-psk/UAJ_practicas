54518fef6e22fac6a9615d520810cb895f354e78
iquintasALT <iquintas@ucm.es>
2023-05-16 15:47:06 +0200

    visualTesting comment

--- G2_PF/ianium/src/ianium/visual_testing/visual_testing.h
+++ G2_PF/ianium/src/ianium/visual_testing/visual_testing.h
@@ -21,6 +21,13 @@ namespace ianium {
 		SDL_Renderer* renderer;
 		SDL_Window* window;
 
+		/// <summary>
+		/// Finds the number of occurrences of a template image inside another one. Uses internally OpenCV to get a mask and separates 
+		/// the connected regions to get how many images were detected
+		/// </summary>
+		/// <param name="imagePath"></param> Original image where the template image is searched on
+		/// <param name="templateImagePath"></param> Template image 
+		/// <returns></returns> Number of occurrences of the template image inside the original image
 		std::vector<std::pair<double, double>> template_matching(std::string imagePath, std::string templateImagePath);
 		void takeScreenshot();
 	};
